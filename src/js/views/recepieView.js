7c1dbcad89163461da86155f39f6ca509562a53a
JpHdz <jpnator18@gmail.com>
2022-12-12 09:48:49 -0600

    Fixed Functional issue

--- src/js/views/recepieView.js
+++ src/js/views/recepieView.js
@@ -1,9 +1,8 @@
 // import icons from 'url:../img/icons.svg';
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { numberToFraction } from '../helpers.js';
 
-console.log(Fraction);
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = 'We could not find that recipe. Please try another one!';
@@ -132,7 +131,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? numberToFraction(ing.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
